f9c5f09e01c08283f2856d0309cf507ddc37421a
Jatin Mahey <maheyjatin2711@gmail.com>
2025-07-10 19:56:20 +0530

    Basix ioredis setup with bullmq installation done

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,5 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default{
-    PORT: process.env.PORT || 4000
+    PORT: process.env.PORT || 4000,
+    REDIS_PORT: Number(process.env.REDIS_PORT) || 6379,
+    REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1'
 };
